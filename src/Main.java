a7fbd0afd8fbbe31db75f5de9ec976f257ca6e93
Olega1981911 <oleg12a@gmail.com>
2022-11-03 23:14:21 +0300

    Ошибку исправил

--- src/Main.java
+++ src/Main.java
@@ -25,9 +25,12 @@ public class Main {
         }
 
         String[] numbers = input.split(regArg[index]);
+        if (numbers.length > 2) {
+            throw new IllegalArgumentException("формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        }
         int output = 0;
         if (converter.isRoman(numbers[0]) == converter.isRoman(numbers[1])) {
-            
+
             boolean isRoman = converter.isRoman(numbers[0]);
             int a, b;
 
@@ -37,6 +40,9 @@ public class Main {
             } else {
                 a = Integer.parseInt(numbers[0]);
                 b = Integer.parseInt(numbers[1]);
+            }
+            if (a <= 0 || a > 10 || b <= 0 || b > 10) {
+                throw new IllegalArgumentException("числа должны быть от 1 до 10");
             }
             switch (reg[index]) {
                 case "+" -> output = a + b;
@@ -48,18 +54,19 @@ public class Main {
             }
             if (isRoman) {
                 System.out.println(converter.intToRoman(output));
-            } else {
-                throw new IllegalArgumentException("т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+
             }
-            
+
+        } else {
+            throw new IllegalArgumentException("в выражении одновременно используются арабские и римские цифры");
         }
         return String.valueOf(output);
     }
-    
+
 
     public static void main(String[] args) {
 
-
+        System.out.println("введите математическое выражение:");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         try {
